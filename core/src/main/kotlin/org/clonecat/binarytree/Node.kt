8ca2184423f062a438e09d7f9d034c759251d174
rubilacz <rubilacz@gmail.com>
2017-12-22 19:01:02 +0800

    support node graphicalization.

--- core/src/main/kotlin/org/clonecat/binarytree/Node.kt
+++ core/src/main/kotlin/org/clonecat/binarytree/Node.kt
@@ -1,3 +1,25 @@
 package org.clonecat.binarytree
 
-class Node<T>(var value: T, var left: Node<T>?, var right: Node<T>?)
+import java.io.PrintWriter
+
+class Node<T>(var value: T, var left: Node<T>?, var right: Node<T>?) {
+
+    override fun toString() = "value: $value, left: ${left?.value}, right: ${right?.value}"
+
+    fun toGraphic(writer: PrintWriter) {
+        doPrint(writer, "", true)
+    }
+
+    private fun doPrint(writer: PrintWriter, prefix: String, isTail: Boolean) {
+        val preCur = if (isTail) "└── " else "├── "
+        writer.println("${prefix}${preCur}${value}")
+        val preChild = prefix + (if (isTail) "    " else "│   ")
+        printChild(left, writer, preChild)
+        printChild(right, writer, preChild)
+    }
+
+    private fun printChild(child: Node<T>?, writer: PrintWriter, prefix: String) {
+        if (child != null) child.doPrint(writer, prefix, false) else writer.println("${prefix}└── null")
+    }
+
+}
--- core/src/test/kotlin/org/clonecat/binarytree/NodeTest.kt
+++ core/src/test/kotlin/org/clonecat/binarytree/NodeTest.kt
@@ -1,16 +1,31 @@
 import org.junit.Test
+import org.junit.Before
 import kotlin.test.*
+import java.io.PrintWriter
 
 import org.clonecat.binarytree.Node
 
 class NodeTest {
 
+    var node: Node<String>? = null
+
+    @Before
+    fun setUp() {
+        node = Node("Node1", Node("SubNode1", null, null), null)
+    }
+
     @Test
-    fun testConstruct() {
-        val node = Node("Node1", Node("SubNode1", null, null), null)
-        assertEquals("Node1", node.value)
-        assertEquals("SubNode1", node.left!!.value)
-        assertEquals(null, node.right)
+    fun testConstructor() {
+        assertEquals("Node1", node?.value)
+        assertEquals("SubNode1", node?.left!!.value)
+        assertEquals(null, node?.right)
+    }
+
+    @Test
+    fun testtoGraphic() {
+        val writer = PrintWriter("build/node_to_graphic.txt")
+        node?.toGraphic(writer)
+        writer.close()
     }
 
 }
